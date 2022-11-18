5d9536e8aca93823c57852f75a071b404b360ce0
bronyaw <osusher@yahoo.com>
2022-11-18 20:48:52 +0300

    2.1.2 lint check, fixed prime logic

--- brain_games/games/game_prime.py
+++ brain_games/games/game_prime.py
@@ -6,13 +6,12 @@ GAME_DESCRIPTION = 'Answer "yes" if given number is prime. '\
 
 
 def is_prime(n):
-    if n == 0 or n == 1:
+    if n < 1:
         return False
-    for i in range(2, int(n / 2)):
+    for i in range(2, int(n / 2) + 1):
         if (n % i) == 0:
             return False
-        else:
-            return True
+    return True
 
 
 def game_run():
